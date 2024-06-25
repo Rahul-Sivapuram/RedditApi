53dadcceabaf83855366c306151738227f401cff
Rahul-Sivapuram <rahul.sivapuram@Technovert.com>
2024-06-25 18:23:58 +0530

    first commit

---
+++ Models/RedditContext.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedditApi.Models;
+
+public partial class RedditContext : DbContext
+{
+    public RedditContext()
+    {
+    }
+
+    public RedditContext(DbContextOptions<RedditContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    public virtual DbSet<Subreddit> Subreddits { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=rahulemsserver.database.windows.net;Initial Catalog=reddit;Persist Security Info=True;User ID=rahulsivapuram;Password=Bobby@2002;Encrypt=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__comments__3213E83FF821D4B8");
+
+            entity.ToTable("comments");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Body)
+                .HasColumnType("text")
+                .HasColumnName("body");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_At");
+            entity.Property(e => e.PostId).HasColumnName("post_Id");
+            entity.Property(e => e.UserId).HasColumnName("user_Id");
+
+            entity.HasOne(d => d.Post).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.PostId)
+                .HasConstraintName("FK__comments__post_I__7A672E12");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__comments__user_I__797309D9");
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__posts__3213E83F57BE9834");
+
+            entity.ToTable("posts");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Body)
+                .HasColumnType("text")
+                .HasColumnName("body");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_At");
+            entity.Property(e => e.SubredditId).HasColumnName("subreddit_Id");
+            entity.Property(e => e.Title)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("updated_At");
+            entity.Property(e => e.Url)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("url");
+            entity.Property(e => e.UserId).HasColumnName("user_Id");
+
+            entity.HasOne(d => d.Subreddit).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.SubredditId)
+                .HasConstraintName("FK__posts__subreddit__74AE54BC");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__posts__user_Id__75A278F5");
+        });
+
+        modelBuilder.Entity<Subreddit>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__subreddi__3213E83F05B339B4");
+
+            entity.ToTable("subreddit");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_At");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Title)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("title");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3213E83F6B34CC23");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_At");
+            entity.Property(e => e.Password)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.Username)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
